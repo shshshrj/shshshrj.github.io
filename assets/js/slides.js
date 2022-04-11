e6ddc3494ee1c17c6e249e1258e66ebd36f667af
shshshrj <95598729+shshshrj@users.noreply.github.com>
2022-04-11 16:28:45 +0900

    hu

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -6,7 +6,8 @@ const prev = document.querySelector('.prev'); //이전 버튼
 const next = document.querySelector('.next'); //다음 버튼
 const slideWidth = 300; //한개의 슬라이드 넓이
 const slideMargin = 100; //슬라이드간의 margin 값
-
+//전체 슬라이드 컨테이너 넓이 설정
+slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
 function moveSlide(num) {
     slides.style.left = -num * 400 + 'px';
     currentIdx = num;
