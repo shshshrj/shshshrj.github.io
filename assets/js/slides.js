fb29a64b60a1eb86aa4868eb8f29cb0e15b15919
shshshrj <95598729+shshshrj@users.noreply.github.com>
2022-04-11 16:21:05 +0900

    up

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -1,50 +1,26 @@
-$('.slider-1 > .page-btns > div').click(function(){
-    var $this = $(this);
-    var index = $this.index();
-    
-    $this.addClass('pop-layer');
-    $this.siblings('.pop-layer').removeClass('pop-layer');
-    
-    var $slider = $this.parent().parent();
-    
-    var $current = $slider.find(' > .slides > div.pop-layer');
-    
-    var $post = $slider.find(' > .slides > div').eq(index);
-    
-    $current.removeClass('pop-layer');
-    $post.addClass('pop-layer');
+const slides = document.querySelector('.slides');//전체 슬라이드 컨테이너
+const slideImg = document.querySelectorAll('.slides li'); //모든 슬라이드들
+let currentIdx = 0; //현재 슬라이드 index
+const slideCount = slideImg.length; // 슬라이드 개수
+const prev = document.querySelector('.prev'); //이전 버튼
+const next = document.querySelector('.next'); //다음 버튼
+const slideWidth = 300; //한개의 슬라이드 넓이
+const slideMargin = 100; //슬라이드간의 margin 값
+//전체 슬라이드 컨테이너 넓이 설정
+slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
+function moveSlide(num) {
+    slides.style.left = -num * 400 + 'px';
+    currentIdx = num;
+}
+
+prev.addEventListener('click', function () {
+    /*첫 번째 슬라이드로 표시 됐을때는 이전 버튼 눌러도 아무런 반응 없게 하기 위해 currentIdx !==0일때만 moveSlide 함수 불러옴 */
+    if (currentIdx !== 0) moveSlide(currentIdx - 1);
 });
 
-// 좌/우 버튼 추가 슬라이더
-$('.slider-1 > .side-btns > div').click(function(){
-    var $this = $(this);
-    var $slider = $this.closest('.slider-1');
-    
-    var index = $this.index();
-    var isLeft = index == 0;
-    
-    var $current = $slider.find(' > .page-btns > div.pop-layer');
-    var $post;
-    
-    if ( isLeft ){
-        $post = $current.prev();
+next.addEventListener('click', function () {
+    /* 마지막 슬라이드로 표시 됐을때는 다음 버튼 눌러도 아무런 반응 없게 하기 위해 currentIdx !==slideCount - 1 일때만 moveSlide 함수 불러옴 */
+    if (currentIdx !== slideCount - 1) {
+        moveSlide(currentIdx + 1);
     }
-    else {
-        $post = $current.next();
-    };
-    
-    if ( $post.length == 0 ){
-        if ( isLeft ){
-            $post = $slider.find(' > .page-btns > div:last-child');
-        }
-        else {
-            $post = $slider.find(' > .page-btns > div:first-child');
-        }
-    };
-    
-    $post.click();
 });
-
-setInterval(function(){
-    $('.slider-1 > .side-btns > div').eq(1).click();
-}, 3000);
